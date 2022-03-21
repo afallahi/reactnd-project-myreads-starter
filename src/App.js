3b2e82de72f9372879120bea96ce0a4ba36dd20c
Afshin <>
2022-03-20 22:13:54 -0400

    bookshelf changer select value

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class BookShelfChanger extends Component {
   render() {
     return (
       <div className="book-shelf-changer">
-      <select>
+      <select value={this.props.shelf}>
         <option value="move" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
